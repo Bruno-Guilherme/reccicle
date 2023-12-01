134e199e2cea08a96bd64f7e8db5d96579e5c263
Bruno Guilherme <dasilvaguilhermebruno@gmail.com>
2023-11-30 21:00:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RecCicle - Monetizando com Reciclagem
+
+## Projeto criado para a residência de software do curso de Sistemas para a Internet em parceria com o Porto Digital.
---
+++ src/script.js
@@ -0,0 +1,26 @@
+const menuBtn = document.querySelector(".menu");
+const menu = document.querySelector("nav");
+
+menuBtn.addEventListener("click", (e) => {
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
+
+const aside = document.querySelector("aside");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+aside.addEventListener("click", (e) => {
+  modal.style.width = "45%";
+  modal.style.height = "90%"
+  aside.style.display = "none";
+});
+
+close.addEventListener("click", (e) => {
+  modal.style.display = "none";
+  aside.style.display = "flex";
+});
